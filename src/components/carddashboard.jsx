1b149ae6a05f8e58d7d7515cab144f2339c03bed
RenggoPandora <renggopandora07@gmail.com>
2025-02-03 14:46:19 +0700

    14.46

--- src/components/carddashboard.jsx
+++ src/components/carddashboard.jsx
@@ -1,24 +1,32 @@
 const isiCard = [
-    { value: 450, label: "Active Users" },
-    { value: 25, label: "Active Class" },
-    { value: 20, label: "Premium Class" }
-  ];
-  
-  function Card() {
-    return (
-      <div className="flex">
-        {isiCard.map((item, index) => (
-          <div
-            key={index}
-            className="bg-[#6148FF] w-1/3 h-40 rounded-3xl px-5 py-5 my-24 mx-10 border-none outline-none"
-          >
-            <h3 className="text-white text-xl font-semibold">{item.value}</h3>
-            <p className="text-white text-2xl font-bold">{item.label}</p>
+  { value: 450, label: "Active Users", color: "#489CFF" },
+  { value: 25, label: "Active Class", color: "#73CA5C" },
+  { value: 20, label: "Premium Class", color: "#6148FF" }
+];
+
+function Card() {
+  return (
+    <div className="flex">
+      {isiCard.map((item, index) => (
+        <div
+          key={index}
+          className={`w-1/3 h-40 rounded-3xl px-5 py-5 my-24 mx-10 border-none outline-none`}
+          style={{ backgroundColor: item.color }}
+          
+        >
+          <div className="bg-white rounded-[25px]">
+          <img src='./logocard.svg' alt="logo" />
           </div>
-        ))}
-      </div>
-    );
-  }
+          <div className='flex'>
+          <h3 className="text-white text-xl font-semibold">{item.value}</h3>
+          <p className="text-white text-2xl font-bold">{item.label}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+  
 
 function carddashboard() {
   return (
